de73e89d11e905d57d6bb539357ed6929bcad788
Ian-Wells <ianwells13@gmail.com>
2015-04-18 14:10:14 -0600

    changes in CLS-footer for link to search
    changes in CLS-footer for link to search

--- public_html/pages/CLS-footer.php
+++ public_html/pages/CLS-footer.php
@@ -23,7 +23,7 @@
       <ul class="nospace linklist">
         <li><a href="CLS-home.php">Home Page</a></li>
 		<li><a href="CLS-login.php">My Library Account</a></li>
-		<li><a href="#">Catalog</a></li>
+		<li><a href="CLS-search.php">Search Catalog</a></li>
         <li><a href="#">Request a Room</a></li>
 		<li><a href="#">About The Library</a></li>
         <li><a href="#">Contact Us</a></li>
